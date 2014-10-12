5bfad263ee54eb61a445853fa37e683ffa7121c1
Luis Osa <luis.osa.gdc@gmail.com>
2014-10-12 13:52:25 +0200

    Perform and cache requests

--- bigquery.py
+++ bigquery.py
@@ -2,18 +2,26 @@ import logging
 
 import httplib2
 from apiclient.discovery import build
+from apiclient.errors import HttpError
+from filecache import filecache
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.file import Storage
 
 
-PROJECT_NUMBER = '413790172023'
+PROJECT_NUMBER = ''
 FLOW = flow_from_clientsecrets(
     'client_secrets.json',
     scope='https://www.googleapis.com/auth/bigquery')
 
 
-def get_most_popular_language(country='France', offset=0):
-    # TODO: split into authorization and retrieval functions
+def get_most_popular_language(project_number, country='France', offset=0):
+    global PROJECT_NUMBER
+    PROJECT_NUMBER = project_number
+    langs = get_languages_by_popularity(country)
+    return langs[offset - 1]
+
+
+def get_authorized_http():
     storage = Storage('bigquery_credentials.dat')
     credentials = storage.get()
     if credentials is None or credentials.invalid:
@@ -23,8 +31,12 @@ def get_most_popular_language(country='France', offset=0):
             FLOW, storage, tools.argparser.parse_args([]))
     http = httplib2.Http()
     http = credentials.authorize(http)
-    # TODO: cache results into a shelve file
-    bigquery_service = build('bigquery', 'v2', http=http)
+    return http
+
+
+@filecache(2 * 30 * 24 * 3600)
+def get_languages_by_popularity(country):
+    bigquery_service = build('bigquery', 'v2', http=get_authorized_http())
     query_data = {'query': (
         "SELECT repository_language AS lang, count(repository_name) AS counts "
         "FROM [githubarchive:github.timeline] "
@@ -33,12 +45,16 @@ def get_most_popular_language(country='France', offset=0):
         "GROUP BY lang "
         "ORDER BY counts DESC ").format(country)}
     query_request = bigquery_service.jobs()
-    query_response = query_request.query(
-        projectId=PROJECT_NUMBER, body=query_data).execute()
     try:
-        first_row = query_response['rows'][offset]
-        language = first_row['f'][0]['v']
-    except KeyError:
-        logging.exception(query_response.keys())
-        language = 'No data'
-    return language
+        query_response = query_request.query(
+            projectId=PROJECT_NUMBER, body=query_data).execute()
+    except HttpError as err:
+        raise RuntimeError(err)
+    languages = []
+    for row in query_response['rows']:
+        try:
+            languages.append(row['f'][0]['v'])
+        except KeyError:
+            logging.exception(query_response.keys())
+            languages.append('No data')
+    return languages
